b198fcb7dc65d8c643709fe333460c9446880a42
Virag Benedek <virag.benedek@BOLYAI.LOCAL>
2024-10-04 12:36:24 +0200

    add form

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
-const array = [
+let array = [
     {
         firstname1: 'Géza',
         firstname2: 'Ferenc',
-        lastname: 'Kocsis'
+        lastname: 'Kocsis',
+        married: true,
+        pet: 'kutya'
     },
     {
         firstname1: 'Mária',
         firstname2: 'Júlia',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: false,
+        pet: 'macska'
     },
     {
         firstname1: 'Ferenc',
-        lastname: 'Balogh'
+        lastname: 'Balogh',
+        married: false,
+        pet: 'teknős'
     },
     {
         firstname1: 'Gábor',
         firstname2: 'Attila',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: true,
+        pet: 'macska'
     },
 ]
-
 const table = document.createElement('table')
 document.body.appendChild(table)
 
@@ -35,22 +42,34 @@ tableheadrow.appendChild(th)
 const th2 = document.createElement('th')
 tableheadrow.appendChild(th2)
 
+const th3 = document.createElement('th')
+tableheadrow.appendChild(th3)
+
+const th4 = document.createElement('th')
+tableheadrow.appendChild(th4)
+
 th.innerHTML="Veznev"
 th2.innerHTML="Kernev"
 th2.colSpan = 2
+th3.innerHTML= "Házas-e"
+th4.innerHTML="Háziállat"
 
 const tablebody = document.createElement('tbody')
 table.appendChild(tablebody)
 for(const person of array){
     const tr = document.createElement('tr')
+
     const lastname = document.createElement('td')
     tablebody.appendChild(tr)
     tr.appendChild(lastname)
     lastname.innerHTML = person.lastname
+
     const firstname = document.createElement('td')
     tablebody.appendChild(tr)
     tr.appendChild(firstname)
     firstname.innerHTML = person.firstname1
+
+    
     
     
     tablebody.appendChild(tr)
@@ -65,6 +84,19 @@ for(const person of array){
         tr.appendChild(firstname2)
 
     }
+
+    const married = document.createElement('td')
+    tablebody.appendChild(tr)
+    tr.appendChild(married)
+    firstname.innerHTML = person.married
+    married.innerHTML = person.married
+
+    const pet = document.createElement('td')
+    tablebody.appendChild(tr)
+    tr.appendChild(pet)
+    firstname.innerHTML = person.pet
+    pet.innerHTML = person.pet
+
     
     tr.addEventListener('click',function(e){
         console.log('click')
