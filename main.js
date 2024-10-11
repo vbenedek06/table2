a282971da28fddfc51f69842185cfc9650b1bfc3
Virag Benedek <virag.benedek@BOLYAI.LOCAL>
2024-10-11 11:50:59 +0200

    Add new element

--- main.js
+++ main.js
@@ -48,82 +48,109 @@ tableheadrow.appendChild(th3)
 const th4 = document.createElement('th')
 tableheadrow.appendChild(th4)
 
-th.innerHTML="Veznev"
-th2.innerHTML="Kernev"
+th.innerHTML = "Veznev"
+th2.innerHTML = "Kernev"
 th2.colSpan = 2
-th3.innerHTML= "Házas-e"
-th4.innerHTML="Háziállat"
+th3.innerHTML = "Házas-e"
+th4.innerHTML = "Háziállat"
 
 const tablebody = document.createElement('tbody')
 table.appendChild(tablebody)
-for(const person of array){
-    const tr = document.createElement('tr')
 
-    const lastname = document.createElement('td')
-    tablebody.appendChild(tr)
-    tr.appendChild(lastname)
-    lastname.innerHTML = person.lastname
-
-    const firstname = document.createElement('td')
-    tablebody.appendChild(tr)
-    tr.appendChild(firstname)
-    firstname.innerHTML = person.firstname1
-
-    
-    
-    
-    tablebody.appendChild(tr)
-    
-
-    if(person.firstname2===undefined){
-        firstname.colSpan = 2
-    }
-    else{
-        const firstname2 = document.createElement('td')
-        firstname2.innerHTML = person.firstname2
-        tr.appendChild(firstname2)
-
-    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-
-    const married = document.createElement('td')
-    tablebody.appendChild(tr)
-    tr.appendChild(married)
-    firstname.innerHTML = person.married
-    married.innerHTML = person.married
-
-    const pet = document.createElement('td')
-    tablebody.appendChild(tr)
-    tr.appendChild(pet)
-    firstname.innerHTML = person.pet
-    pet.innerHTML = person.pet     
-    
-    if (person.married === true){
-        married.innerHTML = "igen"
-    }else{
-        married.p
-    }
-
-    
-    tr.addEventListener('click',function(e){
-        console.log('click')
-       
-        const ez = tablebody.querySelector('.selected')
-        if (ez!=undefined){
-            ez.classList.remove('selected')
-        } 
-        e.currentTarget.classList.add('selected')
-    })
-}
-
-const form = document.getElementById(form)
-form.addEventListener('submit', function(e){
+const form = document.getElementById('form')
+form.addEventListener('submit', function (e) {
     e.preventDefault();
     const lastname = document.getElementById('lastname')
-    const firstname1 = document.getElementById('firstname1')
+    const firstname = document.getElementById('firstname1')
     const firstname2 = document.getElementById('firstname2')
     const pet = document.getElementById('pet')
     const married = document.getElementById('married')
-    
 
-    
+    const lastnamevalue = lastname.value
+    const firstnamevalue = firstname.value
+    let firstname2value = firstname2.value
+    const marriedvalue = married.checked
+    const petvalue = pet.value
+
+    if (firstname2value === ""){
+        firstname2value = undefined
+
+    }
+
+
+    const newperson =
+    {
+        firstname1: firstnamevalue,
+        firstname2: firstname2value,
+        lastname: lastnamevalue,
+        married: marriedvalue,
+        pet: petvalue
+
+    }
+
+    array.push(newperson)
+    console.log(array)
+    tablebody.innerHTML = ""
+    rendertable()
 })
+rendertable()
+function rendertable() {
+    for (const person of array) {
+        const tr = document.createElement('tr')
+
+        const lastname = document.createElement('td')
+        tablebody.appendChild(tr)
+        tr.appendChild(lastname)
+        lastname.innerHTML = person.lastname
+
+        const firstname = document.createElement('td')
+        tablebody.appendChild(tr)
+        tr.appendChild(firstname)
+        firstname.innerHTML = person.firstname1
+
+
+
+
+        tablebody.appendChild(tr)
+
+
+        if (person.firstname2 === undefined) {
+            firstname.colSpan = 2
+        }
+        else {
+            const firstname2 = document.createElement('td')
+            firstname2.innerHTML = person.firstname2
+            tr.appendChild(firstname2)
+
+        }
+
+        const married = document.createElement('td')
+        tablebody.appendChild(tr)
+        tr.appendChild(married)
+        married.innerHTML = person.married
+
+        const pet = document.createElement('td')
+        tablebody.appendChild(tr)
+        tr.appendChild(pet)
+        pet.innerHTML = person.pet
+
+        if (person.married === true) {
+            married.innerHTML = "igen"
+        } else {
+            married.innerHTML = "nem"
+        }
+
+
+        tr.addEventListener('click', function (e) {
+            console.log('click')
+
+            const ez = tablebody.querySelector('.selected')
+            if (ez != undefined) {
+                ez.classList.remove('selected')
+            }
+            e.currentTarget.classList.add('selected')
+        })
+
+    }
+
+}
